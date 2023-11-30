cdd8a09a999ea80a934e3a900995b5854d584d4f
shker <21s21397@mec.edu.om>
2023-11-30 15:22:45 +0400

    some work

---
+++ app/src/main/java/com/example/nahaloman/business_registeration.java
@@ -0,0 +1,14 @@
+package com.example.nahaloman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class business_registeration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_registeration);
+    }
+}
