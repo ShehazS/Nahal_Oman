9ef81dc3b6d617482b6fafacfed28ffdbfe53c8b
shker <21s21397@mec.edu.om>
2023-12-05 18:49:46 +0400

    sell page created

---
+++ app/src/main/java/com/example/nahaloman/sell_page.java
@@ -0,0 +1,14 @@
+package com.example.nahaloman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sell_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_page);
+    }
+}
